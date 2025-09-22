cf03f629a3bab1c076946712697d0f6e6a3a3dba
Jana Sandeyouni <Jnoo66@icloud.com>
2025-09-23 00:12:14 +0300

    add add_customer_account method

--- bank/acme_bank.py
+++ bank/acme_bank.py
@@ -12,8 +12,10 @@ class Casher:
     password = None
     checking_balance = 0.0
     saving_balance = 0.0
+    account_id = None
     
-    def __init__(self, first_name, last_name, password, checking_balance, saving_balance):
+    def __init__(self, account_id, first_name, last_name, password, checking_balance, saving_balance):
+        self.account_id = account_id
         self.first_name = first_name
         self.last_name = last_name
         self.password = password
@@ -21,8 +23,14 @@ class Casher:
         self.saving_balance = saving_balance
     
         
-    def add_customer_account(self):
-        pass
+    def add_customer_account():
+        print('Fill the following fields: ')
+        account_id = int(input('Enter Account Id: '))
+        first_name = input('Enter your First name: ')
+        last_name = input('Enter your Last name: ')
+        password = input('Enter a Password for the account: ')
+        checking_balance = float(input('Enter initial Checking balance: '))
+        checking_balance = float(input('Enter initial Saving balance: '))
     
 # Bank class
 class Bank:
@@ -31,26 +39,31 @@ class Bank:
         print("SELECT A CHOICE")
         print("")
         print("1- Add New Customer")
-        print("2- Log in ")
+        print("2- Login Into Account")
         print("3- Exit")
-        print("")
+        print("-----------------------------------------------><----------------------------------------------")
 
         selection = input("Enter a choice: ")
 
         if(selection == "1"):
-            e1()
+            print(Casher.add_customer_account())
+            print("")
             break
         elif(selection == "2"):
             e2()
+            print("")
             break
         elif(selection == "3"):
             e3()
+            print("")
             break
-        elif(selection == "e7"):
+        elif (selection == "e7"):
             print("Goodbye")
+            print("")
             break
         else:
             print("Invalid choice. Enter a Number choice from '1'-'3'")
+            
             
         
 
